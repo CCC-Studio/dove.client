a11882e001e2b0c1adf79b5a740f70498efa4316
CookiesChen <1109349604@qq.com>
2019-02-15 15:59:54 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LogIn/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LogIn from './components/LogIn.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LogIn
   }
 }
 </script>
@@ -24,5 +23,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body{
+  margin: 0%;
 }
 </style>
---
+++ src/components/LogIn.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <img id="backgorundImg" src="../assets/background.jpg">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LogIn"
+}
+</script>
+
+<style lang="scss" scoped>
+
+div{
+    margin: 0%;
+
+    img{
+        width:100%;
+        height: 100%;
+        background-repeat: no-repeat;
+        
+    }
+}
+
+</style>
+
